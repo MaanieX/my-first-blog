74bb33f9cfa0b73b20c143be5f0e8b2287abfab2
Many <dennisarreman@live.nl>
2017-01-03 21:04:25 +0100

    Added view and template for deatailed blog post as well as CSS for the site.

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Post
+from blog.models import Post
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ["title"]
+    
 admin.site.register(Post)
