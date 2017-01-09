cee0ad2731bfa703f68003fab80f4e2a8320659d
Many <dennisarreman@live.nl>
2017-01-09 10:07:30 +0100

    Added Comment model

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
     search_fields = ["title"]
 
 admin.site.register(Post)
+admin.site.register(Comment)
